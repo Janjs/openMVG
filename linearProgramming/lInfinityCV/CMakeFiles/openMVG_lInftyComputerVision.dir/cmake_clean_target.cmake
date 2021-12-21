56152048957c2eaccf62e5ec9fcc71fd34fb00ad
Jan J. Serra <jan.jime.serra@gmail.com>
2021-12-21 16:43:03 +0100

    Revert "modify SfM CMake file for MacOS support"
    This reverts commit 75eb47c40bea72c9039403297560b00878fcb2a9.

--- linearProgramming/lInfinityCV/CMakeFiles/openMVG_lInftyComputerVision.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../../Darwin-x86_64-RELEASE/libopenMVG_lInftyComputerVision.a"
-)
--- robust_estimation/CMakeFiles/openMVG_robust_estimation.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "../../Darwin-x86_64-RELEASE/libopenMVG_robust_estimation.a"
-)
